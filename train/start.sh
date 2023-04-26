2e6c9b56640ebf4c21590688dd126e09f178abc4
curva12 <48948802+manutd12@users.noreply.github.com>
2023-04-26 19:00:40 +0800

    update taiji

---
+++ train/start.sh
@@ -0,0 +1,1 @@
+bash training_scripts/single_gpu/run_LoRA.sh /apdcephfs_cq3/share_2973545/curvasong/test/output_models/llama/output-lora 3 /apdcephfs_cq3/share_2973545/curvasong/pretrain_model/Belle/BELLE-LLaMA-EXT-7B
