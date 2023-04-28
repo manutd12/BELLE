278bb61aacb07af3c12d45abc4e610e3e37d119e
curva12 <48948802+manutd12@users.noreply.github.com>
2023-04-28 14:57:09 +0800

    update start.sh

--- train/start.sh
+++ train/start.sh
@@ -1,1 +1,1 @@
-bash training_scripts/single_gpu/run_LoRA.sh /apdcephfs_cq3/share_2973545/curvasong/test/output_models/llama/output-lora 3 /apdcephfs_cq3/share_2973545/curvasong/pretrain_model/LLAMA/llama-7b-hf /apdcephfs_cq3/share_2973545/curvasong/dataset/belle/belleMath.json
+bash training_scripts/single_gpu/run_LoRA.sh /apdcephfs_cq3/share_2973545/curvasong/test/output_models/llama/output-lora 3 /apdcephfs_cq3/share_2973545/curvasong/pretrain_model/LLAMA/llama-7b-hf /apdcephfs_cq3/share_2973545/curvasong/dataset/belle/belleMath-dev1K.json
