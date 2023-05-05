fc3d9d404238945e86194ea2927531fde0d5f854
curva12 <48948802+manutd12@users.noreply.github.com>
2023-05-05 15:51:55 +0800

    update start.sh

--- train/start.sh
+++ train/start.sh
@@ -1,1 +1,9 @@
-bash training_scripts/single_gpu/run_LoRA.sh /apdcephfs_cq3/share_2973545/curvasong/test/output_models/llama/output-lora 3 /apdcephfs_cq3/share_2973545/curvasong/pretrain_model/LLAMA/llama-7b-hf /apdcephfs_cq3/share_2973545/curvasong/dataset/belle/belleMath-dev1K.json
+#train
+#bash training_scripts/single_gpu/run_LoRA.sh /apdcephfs_cq3/share_2973545/curvasong/test/output_models/llama/output-lora 3 /apdcephfs_cq3/share_2973545/curvasong/pretrain_model/LLAMA/llama-7b-hf /apdcephfs_cq3/share_2973545/curvasong/dataset/belle/belleMath-dev1K.json
+
+#predict
+python3 prompt_eval.py \
+    --model_name_or_path /apdcephfs_cq3/share_2973545/curvasong/pretrain_model/LLAMA/llama-7b-hf \
+    --finetuned_model_name_or_path /apdcephfs_cq3/share_2973545/curvasong/pretrain_model/LLAMA/llama-7b-hf \
+    --test_file /apdcephfs_cq3/share_2973545/curvasong/dataset/belle/belleMath-dev1K.json \
+    --predictions_file /apdcephfs_cq3/share_2973545/curvasong/test/predictions.json
